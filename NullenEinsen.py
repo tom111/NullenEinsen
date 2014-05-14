3638094c634b0cf368af637ce0df536a76e10001
Thomas Kahle <thomas.kahle@jpberlin.de>
2014-05-14 15:51:40 +0200

    Kommentare im Quelltext

--- NullenEinsen.py
+++ NullenEinsen.py
@@ -7,6 +7,7 @@ from Zustand import Zustand
 Z = Zustand(3)
 
 anleitung = """
+\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
 Anleitung:
 
 Bitte geben sie eine zufällige Folge aus Nullen und Einsen ein.  Nach
@@ -21,6 +22,9 @@ eingeben.
 
 print anleitung
 while True:
+    # Die folgende Logik verarbeitet die Eingabe von entweder Ziffern
+    # oder den Steuerzeichen.  Jede nicht passende Eingabe wird
+    # einfach ignoriert.
     try:
         folge = raw_input ("Bitte eine zufällige Null-Eins Folge eingeben: ")
     except (EOFError, KeyboardInterrupt):
@@ -28,17 +32,24 @@ while True:
         exit (0);
     for zeichen in folge:
         if zeichen == '0' :
+            # Eingabe Null weitergeben an die Logik
             Z.eingabe(0)
             print "Eingabe: 0"
             Z.quote()
         elif zeichen == '1':
+            # Eingabe Eins weitergeben an die Logik
             Z.eingabe(1)
             print "Eingabe 1"
             Z.quote()
-        elif zeichen == 'n': Z = Zustand(3)
+        elif zeichen == 'n':
+            # Eingabe n -> Neustart
+            print anleitung
+            Z = Zustand(3)
         elif zeichen == 'z':
+            # Eingabe z -> 100 Zufallszahlen an die Logik weitergeben
             for i in range (100):
                 Z.eingabe (randint (0,1), still=True)
             Z.quote()
         elif zeichen == 'f':
+            # Eingabe f -> Ausgabe der bisherigen Folge
             print Z.folge
--- Zustand.py
+++ Zustand.py
@@ -6,35 +6,50 @@ from random import randint
 class Zustand:
     "Repräsentiert den Zustand des Programms"
     folge = ""  # Bisherige Eingaben
-    statistik = {}  # Hash mit bisheriger Statistik
+    statistik = {}  # Dictionary mit bisheriger Statistik.  Das
+                    # Dictionary merkt sich zu jeder Kombination von
+                    # 'tiefe' vielen Bits, wie oft diese Kombination
+                    # in der gespeicherten Folge vorkam.
     treffer = 0 # Anzahl richtige Tipps
-    tiefe = 3 # Suchtiefe für die Statistik
+    tiefe = 3 # Suchtiefe für die Statistik, Default = 3
     
     def __init__ (self, suchtiefe):
-        self.folge = ""
-        self.tiefe = suchtiefe
-        self.treffer=0
+        # Initialisiert das Programm mit 
+        self.folge = "" # ... der leeren Folge
+        self.tiefe = suchtiefe # ... der gegebenen Tiefe der Statistik
+        self.treffer=0 # ... keinen bisherigen Treffern
 
-        # Initialisiere dictionary mit Nullen
+        # Initialisiere das dictionary mit Nullen
         for i in range (2**suchtiefe):
-            # Ganzzahl in Bitstring:
+            # Wandle die Ganzzahl in einen Bitstring...
             b = bin(i)[2:]
-            # vorne Mit Nullen auffüllen
+            # ... und fülle vorne Mit Nullen auf.
             b = "".join([str(0) for i in range(self.tiefe-len(b))]) + b
+            # Setze den Wert zu dieser Bitfolge:
             self.statistik[ b ] = 0;
 
     def rate (self, still=False):
-        "Diese Funktion rät die nächste Eingabe und hat natürlich keine Argumente außer dem internen Zustand zur Verfügung."
-        # Falls nicht genug Daten: Zufall
+        # Diese Funktion rät die nächste Eingabe und hat natürlich
+        # keine Argumente außer dem internen Zustand zur Verfügung.
+        # Die Option "still" steuert ob informative Ausgaben erfolgen
+        # sollen (Standard=nein).
+        
+        # Falls nicht genug Daten vorliegen da Folge zu kurz: Würfeln
         if len(self.folge) < self.tiefe:
             tipp = randint(0,1)
             if not still: print ("Tipp: " + str(tipp))
             return tipp
+
+        # Falls die Folge lang genug ist, betrachte letzte tiefe-1
+        # bits und vergleiche welches bit darauf häufiger folgte:
         lastbits = self.folge[ -(self.tiefe-1): ]
-        # print "lastbits:"
-        # print lastbits
-        # Wähle das häufigere nächste bit
-        if self.statistik[ lastbits + str(0) ] > self.statistik [ lastbits + str(1) ]:
+        if self.statistik[ lastbits + str(0) ] == self.statistik [ lastbits + str(1) ]:
+            # Bei Gleichstand: Zufall
+            tipp = randint(0,1)
+            if not still: print ("Tipp: " + str(tipp))
+            return tipp
+        # Ansonsten: Wähle das häufigere nächste bit
+        elif self.statistik[ lastbits + str(0) ] > self.statistik [ lastbits + str(1) ]:
             if not still: print "Tipp: 0"
             return 0
         else:
@@ -42,19 +57,23 @@ class Zustand:
             return 1
     
     def eingabe (self, bit, still=False):
-        "Rate die nächste Eingabe"
+        # Diese Funktion verarbeitet eine Eingabe
+
+        # Bestimme zunächst den Tipp
         tipp = self.rate(still);
+
+        # Werte Tipp aus:
         if tipp == int(bit):
             self.treffer += 1;
+
+        # Speichere neue Eingabe in der Folge
         self.folge += str(bit)
+        # Und aktualisiere Statistik:
         if len(self.folge) >= self.tiefe:
             self.statistik [ self.folge[-self.tiefe:] ] += 1;
 
-        # print "Folge:"
-        # print self.folge
-        # print "Statistik:"
-        # print self.statistik
-
     def quote(self):
+        # Diese Funktion gibt die aktuelle Quote aus
         print " ".join(["Aktuelle Quote:", str(self.treffer), "Treffer aus",
                         str(len(self.folge)), "Versuchen:", str(int(100*self.treffer / len(self.folge))),"%"])
+        
